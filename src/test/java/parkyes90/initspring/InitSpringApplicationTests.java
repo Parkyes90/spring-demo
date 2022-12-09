b57ec2cf6d03408efa66666f35b98c495f06c811
parkyes90 <parkyes90@gmail.com>
2022-12-09 18:42:31 +0900

    init spring

---
+++ src/test/java/parkyes90/initspring/InitSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package parkyes90.initspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
