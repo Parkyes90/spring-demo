e1e379488a1d52846cb8f94c225e2914fa2a9cb5
parkyes90 <parkyes90@gmail.com>
2022-12-11 23:33:17 +0900

    use jdbc repository

--- src/main/java/parkyes90/initspring/SpringConfig.java
+++ src/main/java/parkyes90/initspring/SpringConfig.java
@@ -1,13 +1,25 @@
 package parkyes90.initspring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import parkyes90.initspring.repository.JdbcMemberRepository;
 import parkyes90.initspring.repository.MemberRepository;
-import parkyes90.initspring.repository.MemoryMemberRepository;
 import parkyes90.initspring.service.MemberService;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -15,6 +27,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
