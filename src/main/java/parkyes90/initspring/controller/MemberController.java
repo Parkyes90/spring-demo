4c21502b0f1619ecf6ff2bda19a30a28f004c3af
parkyes90 <parkyes90@gmail.com>
2022-12-11 11:39:13 +0900

    Add create controller

--- src/main/java/parkyes90/initspring/controller/MemberController.java
+++ src/main/java/parkyes90/initspring/controller/MemberController.java
@@ -3,6 +3,8 @@ package parkyes90.initspring.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import parkyes90.initspring.domain.Member;
 import parkyes90.initspring.service.MemberService;
 
 @Controller
@@ -17,5 +19,14 @@ public class MemberController {
     @GetMapping("/members/new")
     public String createForm() {
         return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/";
     }
 }
