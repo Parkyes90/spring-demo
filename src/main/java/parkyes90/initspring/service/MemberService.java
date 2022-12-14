d110a0cbdcfd186d9477aff026c7f80f9f935127
parkyes90 <parkyes90@gmail.com>
2022-12-14 22:48:15 +0900

    Add measure time

--- src/main/java/parkyes90/initspring/service/MemberService.java
+++ src/main/java/parkyes90/initspring/service/MemberService.java
@@ -23,9 +23,18 @@ public class MemberService {
      */
     public Long join(Member member) {
         // 같은 이름이 있는 중복 회원 X
-        validateDuplicateMember(member);
-        memberRepository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+
+        try {
+            validateDuplicateMember(member);
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
+
     }
 
     private void validateDuplicateMember(Member member) {
